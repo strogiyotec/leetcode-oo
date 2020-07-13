32987f265334fc387fc833d80ca6a48b109500e8
strogiyotec <almas337519@gmail.com>
2020-07-13 09:56:54 -0700

    Longest plalindrom

---
+++ src/main/java/leetcode/oo/dp/LPS.java
@@ -0,0 +1,57 @@
+package leetcode.oo.dp;
+
+public final class LPS {
+
+    String dynamicApproach(final String input) {
+        int end = 0;
+        int start = 0;
+        for (int i = 0; i < input.length(); i++) {
+            final int length = Math.max(
+                    this.expand(input, i, i),
+                    this.expand(input, i, i + 1)
+            );
+            if (length > end - start) {
+                start = i - ((length - 1) / 2);
+                end = i + (length / 2);
+            }
+        }
+        return input.substring(start, end + 1);
+    }
+
+    private int expand(final String input, int left, int right) {
+        while (left >= 0 && right < input.length() && input.charAt(left) == input.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    String bruteForce(final String word) {
+        String max = "";
+
+        for (int i = 1; i < word.length(); i++) {
+            final StringBuilder builder = new StringBuilder();
+            for (int j = 0; j < i; j++) {
+                builder.append(word.charAt(j));
+            }
+            final String subString = builder.toString();
+            if (this.isPalindrome(subString)) {
+                if (subString.length() > max.length()) {
+                    max = subString;
+                }
+            }
+        }
+        return max;
+    }
+
+    private boolean isPalindrome(final String word) {
+        int start = 0;
+        int end = word.length() - 1;
+        while (end >= start) {
+            if (word.charAt(start++) != word.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/leetcode/oo/dp/LPSTestCase.java
@@ -0,0 +1,32 @@
+package leetcode.oo.dp;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public final class LPSTestCase {
+
+    private LPS alg;
+
+    @Before
+    public void init(){
+        this.alg = new LPS();
+    }
+
+    @Test
+    public void test(){
+        Assert.assertEquals(
+                this.alg.dynamicApproach("cbbd"),
+                "bb"
+        );
+        Assert.assertEquals(
+                this.alg.dynamicApproach("babad"),
+                "aba"
+        );
+        Assert.assertEquals(
+                this.alg.dynamicApproach("racecar"),
+                "racecar"
+        );
+    }
+
+}
