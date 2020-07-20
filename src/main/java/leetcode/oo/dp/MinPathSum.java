3873d37ff05e8d4838ed92d83380718608528b87
strogiyotec <almas337519@gmail.com>
2020-07-20 09:55:31 -0700

    more readable min path sum

--- src/main/java/leetcode/oo/dp/MinPathSum.java
+++ src/main/java/leetcode/oo/dp/MinPathSum.java
@@ -9,17 +9,17 @@ final class MinPathSum {
 
     int minPathSum(int[][] grid) {
         final int[][] dp = new int[grid.length][grid[0].length];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        for (int j = 1; j < dp[0].length; j++) {
+            dp[0][j] = grid[0][j] + dp[0][j - 1];
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
                 dp[i][j] = grid[i][j];
-                //then check left and top
-                if (i > 0 && j > 0) {
-                    dp[i][j] += Math.min(dp[i][j - 1], dp[i - 1][j]);
-                } else if (i >= 1) {
-                    dp[i][j] += dp[i - 1][j];
-                } else if (j >= 1) {
-                    dp[i][j] += dp[i][j - 1];
-                }
+                dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
             }
         }
         return dp[dp.length - 1][dp[0].length - 1];
