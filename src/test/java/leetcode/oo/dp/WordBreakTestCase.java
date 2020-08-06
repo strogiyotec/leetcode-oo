d4cd708c00797d9572d318790fdff6f4a98e12a7
strogiyotec <almas337519@gmail.com>
2020-08-06 10:04:54 -0700

    word break

---
+++ src/test/java/leetcode/oo/dp/WordBreakTestCase.java
@@ -0,0 +1,26 @@
+package leetcode.oo.dp;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public final class WordBreakTestCase {
+
+    private WordBreak alg;
+
+    /**
+     * Init.
+     */
+    @Before
+    public void init() {
+        this.alg = new WordBreak();
+    }
+
+    @Test
+    public void test() {
+        Assert.assertTrue(this.alg.wordBreak("leetcode", Arrays.asList("leet","code")));
+        Assert.assertFalse(this.alg.wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
+    }
+}
