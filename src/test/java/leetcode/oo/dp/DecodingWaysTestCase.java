4522d727fba217ce0015a3d7859abe20c91c4533
strogiyotec <almas337519@gmail.com>
2020-08-10 08:25:29 -0700

    more decoding test cases

--- src/test/java/leetcode/oo/dp/DecodingWaysTestCase.java
+++ src/test/java/leetcode/oo/dp/DecodingWaysTestCase.java
@@ -12,12 +12,20 @@ public final class DecodingWaysTestCase {
      * Init.
      */
     @Before
-    public void init(){
+    public void init() {
         this.alg = new DecodingWays();
     }
 
     @Test
-    public void test(){
+    public void test() {
+        Assert.assertEquals(
+                this.alg.numDecodings("012"),
+                0
+        );
+        Assert.assertEquals(
+                this.alg.numDecodings("10"),
+                1
+        );
         Assert.assertEquals(
                 this.alg.numDecodings("01"),
                 0
