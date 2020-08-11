69dea9a18e24a1761b84d3bf8f181e438bb5da96
strogiyotec <almas337519@gmail.com>
2020-08-11 11:13:19 -0700

    optimize con change

--- src/main/java/leetcode/oo/dp/backtracking/CoinChange.java
+++ src/main/java/leetcode/oo/dp/backtracking/CoinChange.java
@@ -2,7 +2,6 @@ package leetcode.oo.dp.backtracking;
 
 import java.util.Arrays;
 
-
 /**
  * Coin Change.
  * See {@link <a href ="https://leetcode.com/problems/coin-change/">https://leetcode.com/problems/coin-change/</a>}
@@ -11,13 +10,15 @@ final class CoinChange {
 
 
     int count(final int[] coins, final int sum) {
-        final int[] dp = new int[sum + 1];
-        Arrays.fill(dp, sum + 1);
+        final int max = sum + 1;
+        final int[] dp = new int[max];
+        Arrays.fill(dp, max);
         dp[0] = 0;
-        for (int i = 1; i <= sum; i++) {
+        for (int i = 1; i < max; i++) {
             for (final int coin : coins) {
-                if (i >= coin) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                final int diff = i - coin;
+                if (diff >= 0) {
+                    dp[i] = Math.min(dp[i], dp[diff] + 1);
                 }
             }
         }
