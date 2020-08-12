69a1fc7b761e145e3faa48d86d6feba1f5ae5426
strogiyotec <almas337519@gmail.com>
2020-08-12 09:44:09 -0700

    improve reversed list

--- src/main/java/leetcode/oo/reverse/ReversedList.java
+++ src/main/java/leetcode/oo/reverse/ReversedList.java
@@ -6,7 +6,7 @@ import leetcode.oo.ListNode;
  * Reverse Linked List.
  * See {@link <a href ="https://leetcode.com/problems/reverse-linked-list/">https://leetcode.com/problems/reverse-linked-list/</a>}
  */
-public final class ReversedList {
+final class ReversedList {
 
     /**
      * Reverse given list.
@@ -14,7 +14,7 @@ public final class ReversedList {
      * @param head Head of list
      * @return Reversed list
      */
-    public ListNode reverseList(final ListNode head) {
+    ListNode reverseList(final ListNode head) {
         if (head.next == null) {
             return head;
         }
@@ -22,17 +22,5 @@ public final class ReversedList {
         head.next.next = head;
         head.next = null;
         return reversed;
-
-    }
-
-    /**
-     * Check if node is the tail.
-     *
-     * @param node List node
-     * @return True if given node is null
-     * or it doesn't have next element
-     */
-    private boolean isLast(final ListNode node) {
-        return node == null || node.next == null;
     }
 }
