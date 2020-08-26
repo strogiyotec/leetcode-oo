565c2c92774714ef72550e2bcb0316515359b37d
strogiyotec <almas337519@gmail.com>
2020-08-26 10:08:03 -0700

    modify max slid window

--- src/main/java/leetcode/oo/window/MaxSlidingWindow.java
+++ src/main/java/leetcode/oo/window/MaxSlidingWindow.java
@@ -1,13 +1,12 @@
 package leetcode.oo.window;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.PriorityQueue;
 
 /**
  * Maximum sliding window.
  * See {@link <a href ="https://leetcode.com/problems/sliding-window-maximum/">https://leetcode.com/problems/sliding-window-maximum/</a>}
  */
-public final class MaxSlidingWindow {
+final class MaxSlidingWindow {
 
     /**
      * Find max sliding window array values.
@@ -16,75 +15,22 @@ public final class MaxSlidingWindow {
      * @param limit Limit
      * @return Array with max sliding values
      */
-    public int[] maxSlidingWindow(final int[] nums, final int limit) {
-        final int length = nums.length;
-        if (length == 0) {
+    int[] maxSlidingWindow(final int[] nums, final int limit) {
+        if (nums.length == 0) {
             return new int[0];
         }
-        final int[] result = new int[length - limit + 1];
-        final Deque<Integer> indexesStorage = new ArrayDeque<>(limit + 1);
-        int resultIndex = 0;
-        for (int index = 0; index < length; index++) {
-            while (this.firstIndexExceedLimit(indexesStorage, limit, index)) {
-                indexesStorage.pollFirst();
+        int index = 0;
+        int[] answer = new int[nums.length - limit + 1];
+        int position = 0;
+        final PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+        while (position < answer.length) {
+            while (queue.size() != limit) {
+                queue.offer(nums[index++]);
             }
-            while (this.lastValueSmaller(indexesStorage, nums, index)) {
-                indexesStorage.pollLast();
-            }
-            indexesStorage.offerLast(index);
-            if (index >= limit - 1) {
-                result[resultIndex++] = nums[indexesStorage.peekFirst()];
-            }
+            answer[position] = queue.peek();
+            queue.remove(nums[position++]);
         }
-        return result;
-    }
-
-    /**
-     * Check if number with last index is smaller than value by current index.
-     * The first index in index storage is always the index that
-     * represents the biggest value in nums array by given index
-     * If value by last index from indexStorage is smaller than
-     * value by current iterator index than last index
-     * must be deleted from indexStorage
-     * In this case storage will store only indexes of
-     * biggest values
-     *
-     * @param indexesStorage Storage of indexes
-     * @param nums           Array of numbers
-     * @param currentIndex   Current iterator index
-     * @return True if value by last index of storage
-     * is smaller than value by currentIndex
-     */
-    private boolean lastValueSmaller(
-            final Deque<Integer> indexesStorage,
-            final int[] nums,
-            final int currentIndex
-    ) {
-        return !indexesStorage.isEmpty()
-                && nums[indexesStorage.peekLast()] < nums[currentIndex];
-    }
-
-    /**
-     * Check if first index in indexStorage doesn't exceed limit.
-     * As only numbers with limited range should be checked
-     * we should delete the first index if
-     * they are outside of this limit
-     * For example: limit is 3 and storage contains
-     * the following indexes (1,[2,3,4])
-     * In this case first index should be deleted because
-     * it's outside of range
-     *
-     * @param indexesStorage Storage of indexes
-     * @param limit          Limit
-     * @param currentIndex   Current iterator index
-     * @return True if limit exceeded
-     */
-    private boolean firstIndexExceedLimit(
-            final Deque<Integer> indexesStorage,
-            final int limit,
-            final int currentIndex
-    ) {
-        return !indexesStorage.isEmpty()
-                && indexesStorage.peekFirst() < currentIndex + 1 - limit;
+        return answer;
     }
 }
+
