9ddc22e62bfade4652343236c033fcfbb7295cbd
strogiyotec <almas337519@gmail.com>
2020-09-11 09:09:20 -0700

    number of islands fix

--- src/main/java/leetcode/oo/graphs/NumberOfIslands.java
+++ src/main/java/leetcode/oo/graphs/NumberOfIslands.java
@@ -10,7 +10,7 @@ final class NumberOfIslands {
         final Dfs dfs = new Dfs() {
             @Override
             public int count(final int i, final int j) {
-                if (i < 0 || j < 0 || i >= grid.length || j >= grid.length || grid[i][j] == '0')
+                if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
                     return 0;
                 grid[i][j] = '0';
                 this.count(i + 1, j);
