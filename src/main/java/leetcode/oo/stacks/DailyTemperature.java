a2d3a1c42cc1e31901a9c7cdd959965735b26286
strogiyotec <almas337519@gmail.com>
2021-01-20 11:11:29 -0800

    improve daily temp

--- src/main/java/leetcode/oo/stacks/DailyTemperature.java
+++ src/main/java/leetcode/oo/stacks/DailyTemperature.java
@@ -5,25 +5,22 @@ import java.util.Stack;
 //https://leetcode.com/problems/daily-temperatures/
 final class DailyTemperature {
 
+    //73, 74, 75, 71, 69, 72, 76, 73
     int[] dailyTemperatures(final int[] temperature) {
-        final int[] solution = new int[temperature.length];
+        final int[] answer = new int[temperature.length];
         final Stack<Integer> stack = new Stack<>();
         stack.push(temperature.length - 1);
         for (int i = temperature.length - 2; i >= 0; i--) {
-            if (temperature[i] < temperature[stack.peek()]) {
-                solution[i] = stack.peek() - i;
+            while (!stack.isEmpty() && temperature[i] >= temperature[stack.peek()]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                answer[i] = 0;
             } else {
-                while (!stack.isEmpty() && temperature[i] >= temperature[stack.peek()]) {
-                    stack.pop();
-                }
-                if (stack.isEmpty()) {
-                    solution[i] = 0;
-                } else {
-                    solution[i] = stack.peek() - i;
-                }
+                answer[i] = stack.peek() - i;
             }
             stack.push(i);
         }
-        return solution;
+        return answer;
     }
 }
