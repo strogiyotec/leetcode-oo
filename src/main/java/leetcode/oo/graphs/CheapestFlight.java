f9fd6ad66ace2d707d7f93bb7ceadb0ff76f55b5
strogiyotec <almas337519@gmail.com>
2021-01-26 09:12:55 -0800

    improve cheapest flight

--- src/main/java/leetcode/oo/graphs/CheapestFlight.java
+++ src/main/java/leetcode/oo/graphs/CheapestFlight.java
@@ -1,31 +1,20 @@
 package leetcode.oo.graphs;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
-public final class CheapestFlight {
+//https://leetcode.com/problems/cheapest-flights-within-k-stops/
+final class CheapestFlight {
 
-    public static void main(final String[] args) {
-        System.out.println(new CheapestFlight().findCheapestPrice(
-            3,
-            new int[][]{
-                {0, 1, 100}, {1, 2, 100}, {0, 2, 500}
-            },
-            0,
-            2,
-            1
-        ));
-    }
-
-    @SuppressWarnings("ALL")
-    public int findCheapestPrice(final int n, final int[][] flights, final int src, final int dst, int stops) {
+    int findCheapestPrice(final int n, final int[][] flights, final int src, final int dst, int stops) {
         final Map<Integer, Map<Integer, Integer>> adjacent = new HashMap<>();
         for (final int[] flight : flights) {
             adjacent.putIfAbsent(flight[0], new HashMap<>());
             adjacent.get(flight[0]).put(flight[1], flight[2]);
         }
-        final PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        final PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
         queue.add(new int[]{src, 0, stops + 1});
         while (!queue.isEmpty()) {
             final int[] poll = queue.poll();
@@ -38,7 +27,7 @@ public final class CheapestFlight {
             if (stop != 0) {
                 if (adjacent.containsKey(city)) {
                     for (final Integer key : adjacent.get(city).keySet()) {
-                        queue.add(new int[]{key,adjacent.get(city).get(key)+price,stop-1});
+                        queue.add(new int[]{key, adjacent.get(city).get(key) + price, stop - 1});
                     }
                 }
             }
