1b52fceae434236a93acca5188c2df496eb25964
strogiyotec <almas337519@gmail.com>
2021-01-27 09:45:19 -0800

    improve min height tree

--- src/main/java/leetcode/oo/tree/MinHeightTree.java
+++ src/main/java/leetcode/oo/tree/MinHeightTree.java
@@ -1,46 +1,43 @@
 package leetcode.oo.tree;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 //https://leetcode.com/problems/minimum-height-trees/
 public final class MinHeightTree {
 
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if (n == 1) {
-            return Collections.singletonList(1);
-        }
-
-        final List<Set<Integer>> adjacentList = new ArrayList<>();
+        final Map<Integer, Set<Integer>> adjacent = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            adjacentList.add(new HashSet<>());
+            adjacent.put(i, new HashSet<>());
         }
         for (final int[] edge : edges) {
-            adjacentList.get(edge[0]).add(edge[1]);
-            adjacentList.get(edge[1]).add(edge[0]);
+            adjacent.get(edge[0]).add(edge[1]);
+            adjacent.get(edge[1]).add(edge[0]);
         }
         final List<Integer> leaves = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (adjacentList.get(i).size() == 1) {
+            if (adjacent.get(i).size() == 1) {
                 leaves.add(i);
             }
         }
-        int remain = n;
-        while (remain > 2) {
-            remain -= leaves.size();
-            final List<Integer> nextLevelLeaves = new ArrayList<>();
+        int size = n;
+        while (size > 2) {
+            size -= leaves.size();
+            final List<Integer> nextLevel = new ArrayList<>();
             for (final Integer leaf : leaves) {
-                final Integer parent = adjacentList.get(leaf).iterator().next();
-                adjacentList.get(parent).remove(leaf);
-                if (adjacentList.get(parent).size() == 1) {
-                    nextLevelLeaves.add(parent);
+                final Integer next = adjacent.get(leaf).iterator().next();
+                adjacent.get(next).remove(leaf);
+                if (adjacent.get(next).size() == 1) {
+                    nextLevel.add(next);
                 }
             }
             leaves.clear();
-            leaves.addAll(nextLevelLeaves);
+            leaves.addAll(nextLevel);
         }
         return leaves;
     }
