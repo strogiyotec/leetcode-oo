a1507934970b36bbc45ea3759a262764176331fb
strogiyotec <almas337519@gmail.com>
2021-02-10 07:57:47 -0800

    more readable task scheduler

--- src/main/java/leetcode/oo/dp/greedy/TaskScheduler.java
+++ src/main/java/leetcode/oo/dp/greedy/TaskScheduler.java
@@ -15,27 +15,30 @@ final class TaskScheduler {
 
     int leastInterval(final char[] tasks, final int limit) {
         final Map<Character, Integer> cnt = new HashMap<>();
-        for (int i = 0; i < tasks.length; i++) {
-            cnt.merge(tasks[i], 1, Integer::sum);
+        for (final char task : tasks) {
+            cnt.merge(task, 1, Integer::sum);
         }
+
         final PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(value -> (int) value).reversed());
         queue.addAll(cnt.values());
         int cycles = 0;
         while (!queue.isEmpty()) {
-            final List<Integer> list = new ArrayList<>();
-            int i = 0;
-            while (!queue.isEmpty() && i <= limit) {
-                final Integer poll = queue.poll();
-                i++;
-                cycles++;
-                if (poll - 1 > 0) {
-                    list.add(poll - 1);
+            final List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i <= limit; i++) {
+                if (queue.isEmpty()) {
+                    cycles++;
+                } else {
+                    final Integer poll = queue.poll();
+                    cycles++;
+                    if (poll > 1) {
+                        temp.add(poll - 1);
+                    }
+                }
+                if(temp.isEmpty()){
+                    break;
                 }
             }
-            if (i <= limit && !list.isEmpty()) {
-                cycles += limit - i + 1;
-            }
-            queue.addAll(list);
+            queue.addAll(temp);
         }
         return cycles;
     }
