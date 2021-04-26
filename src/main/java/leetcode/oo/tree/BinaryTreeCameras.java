fc7aae65b541061b49ad4a65095d8b3304065bd8
strogiyotec <almas337519@gmail.com>
2021-04-26 07:36:26 -0700

    binary tree cameras reimplement

--- src/main/java/leetcode/oo/tree/BinaryTreeCameras.java
+++ src/main/java/leetcode/oo/tree/BinaryTreeCameras.java
@@ -4,42 +4,31 @@ package leetcode.oo.tree;
 final class BinaryTreeCameras {
 
     int minCameraCover(final PlainTree root) {
-        final Status dfs = this.dfs(root);
-        if (dfs.state == State.NOT) {
-            dfs.cameras++;
+        final int[] cnt = new int[1];
+        final State dfs = this.dfs(root, cnt);
+        if (dfs == State.NOT) {
+            cnt[0]++;
         }
-        return dfs.cameras;
+        return cnt[0];
     }
 
-    private Status dfs(final PlainTree root) {
+    private State dfs(final PlainTree root, final int[] cnt) {
         if (root == null) {
-            return new Status(State.MONITORED, 0);
+            return State.MONITORED;
         }
-        final Status left = this.dfs(root.left);
-        final Status right = this.dfs(root.right);
-        final Status current = new Status(State.NOT, left.cameras + right.cameras);
-        if (left.state == State.NOT || right.state == State.NOT) {
-            current.state = State.CAMERA;
-            current.cameras++;
-        } else if (left.state == State.CAMERA || right.state == State.CAMERA) {
-            current.state = State.MONITORED;
+        final State left = this.dfs(root.left, cnt);
+        final State right = this.dfs(root.right, cnt);
+        State current = State.NOT;
+        if (left == State.NOT || right == State.NOT) {
+            cnt[0]++;
+            current = State.CAMERA;
+        } else if (left == State.CAMERA || right == State.CAMERA) {
+            current = State.MONITORED;
         }
         return current;
     }
 
     private enum State {
         MONITORED, NOT, CAMERA
-    }
-
-    private static class Status {
-        BinaryTreeCameras.State state;
-
-        int cameras;
-
-        public Status(final BinaryTreeCameras.State state, final int cameras) {
-            this.state = state;
-            this.cameras = cameras;
-        }
-
     }
 }
--- src/test/java/leetcode/oo/tree/BinaryTreeCamerasTestCase.java
+++ src/test/java/leetcode/oo/tree/BinaryTreeCamerasTestCase.java
@@ -28,4 +28,17 @@ public final class BinaryTreeCamerasTestCase {
             this.alg.minCameraCover(tree)
         );
     }
+
+    @Test
+    public void test2() {
+        final PlainTree tree = new PlainTree(1);
+        tree.right = new PlainTree(2);
+        tree.right.left = new PlainTree(3);
+        tree.right.right = new PlainTree(4);
+        tree.right.right.right = new PlainTree(5);
+        Assert.assertEquals(
+            2,
+            this.alg.minCameraCover(tree)
+        );
+    }
 }
