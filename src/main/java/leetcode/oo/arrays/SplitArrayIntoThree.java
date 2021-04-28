2c252f09926e6df247f4916bfa7613ddd578d3d2
strogiyotec <almas337519@gmail.com>
2021-04-28 09:29:38 -0700

    split array into three , without mode

--- src/main/java/leetcode/oo/arrays/SplitArrayIntoThree.java
+++ src/main/java/leetcode/oo/arrays/SplitArrayIntoThree.java
@@ -4,51 +4,50 @@ package leetcode.oo.arrays;
 final class SplitArrayIntoThree {
 
     int waysToSplit(int[] nums) {
-
-        final int MOD = (int) (1.0e9 + 7);
-
-        final int N = nums.length;
-
-        // prefix array
-        final int[] prefixSum = new int[N];
+        final int[] prefixSum = new int[nums.length];
         prefixSum[0] = nums[0];
-        for (int i = 1; i < N; ++i) {
+        for (int i = 1; i < nums.length; i++) {
             prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
         int res = 0;
-        for (int i = 1; i < prefixSum.length; i++) {
-            final int leftIndex = this.help(prefixSum[i - 1], i, true, prefixSum);
-            final int rightIndex = this.help(prefixSum[i - 1], i, false, prefixSum);
-            if (leftIndex != -1 && rightIndex != -1) {
-                res = (res + rightIndex - leftIndex + 1) % MOD;
+        for (int i = 1; i < nums.length; i++) {
+            final int middleLeft = this.search(prefixSum[i - 1], prefixSum, nums, i, true);
+            final int middleRight = this.search(prefixSum[i - 1], prefixSum, nums, i, false);
+            if (middleLeft != -1 && middleRight != -1) {
+                res += middleRight - middleLeft + 1;
             }
         }
-
         return res;
     }
 
-    private int help(final int leftSum, final int index, final boolean shrinkLeft, final int[] prefixSum) {
-        int left = index;
-        int right = prefixSum.length - 2;
-        int res = -1;
-        while (left <= right) {
-            final int middle = (right - left) / 2 + left;
-            final int middleSum = prefixSum[middle] - prefixSum[index - 1];
-            final int rightSum = prefixSum[prefixSum.length - 1] - prefixSum[middle];
+    private int search(
+        final int leftSum,
+        final int[] prefixSum,
+        final int[] nums,
+        final int index,
+        final boolean decreaseMiddle
+    ) {
+        int middleLeft = index;
+        int middleRight = nums.length - 2;
+        int range = -1;
+        while (middleLeft <= middleRight) {
+            int middlePoint = (middleRight - middleLeft) / 2 + middleLeft;
+            int middleSum = prefixSum[middlePoint] - prefixSum[index - 1];
+            int rightSum = prefixSum[prefixSum.length - 1] - prefixSum[middlePoint];
             if (leftSum <= middleSum && middleSum <= rightSum) {
-                res = middle;
-                if (shrinkLeft) {
-                    right = middle - 1;
+                range = middlePoint;
+                if (decreaseMiddle) {
+                    middleRight = middlePoint - 1;
                 } else {
-                    left = middle + 1;
+                    middleLeft = middlePoint + 1;
                 }
             } else if (leftSum > middleSum) {
-                left = middle + 1;
+                middleLeft = middlePoint + 1;
             } else {
-                right = middle - 1;
+                middleRight = middlePoint - 1;
             }
         }
-        return res;
+        return range;
     }
 
 }
