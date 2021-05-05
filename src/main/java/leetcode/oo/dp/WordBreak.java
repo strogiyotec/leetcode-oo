a080b3254eded5c792151a4477c001351a22aedb
strogiyotec <almas337519@gmail.com>
2021-05-05 07:20:59 -0700

    simplify

--- src/main/java/leetcode/oo/dp/WordBreak.java
+++ src/main/java/leetcode/oo/dp/WordBreak.java
@@ -14,28 +14,27 @@ final class WordBreak {
 
     boolean wordBreak(final String line, final List<String> wordDict) {
         final Set<String> set = new HashSet<>(wordDict);
-        final Map<String, Boolean> memo = new HashMap<>(set.size());
-        return this.validDict(set, line,memo);
+        final Map<String, Boolean> memo = new HashMap<>(wordDict.size());
+        return this.found(line, set, memo);
+
     }
 
-    private boolean validDict(final Set<String> set, final String line, final Map<String, Boolean> memo) {
-        if(memo.containsKey(line)){
-            return memo.get(line);
-        }
-        if (line.isEmpty()) {
+    private boolean found(final String line, final Set<String> set, final Map<String, Boolean> memo) {
+        if(line.isEmpty()){
             return true;
         }
-        boolean found = false;
-        for (final String word : set) {
-            if (line.length() >= word.length() && line.substring(0, word.length()).equals(word)) {
-                final String substr = line.substring(word.length());
-                found = this.validDict(set, substr, memo);
-            }
-            if (found) {
-                return true;
+        if (memo.containsKey(line)) {
+            return memo.get(line);
+        }
+        for (int i = 0; i <= line.length(); i++) {
+            final String sub = line.substring(0, i);
+            if (set.contains(sub)) {
+                if (this.found(line.substring(sub.length()), set, memo)) {
+                    return true;
+                }
             }
         }
-        memo.put(line,false);
+        memo.put(line, false);
         return false;
     }
 
