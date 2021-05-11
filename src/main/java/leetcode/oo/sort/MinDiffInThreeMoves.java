0fd351df91989d52c684771f9033793fd02d4b3f
strogiyotec <almas337519@gmail.com>
2021-05-11 07:34:25 -0700

    improve min diff

--- src/main/java/leetcode/oo/sort/MinDiffInThreeMoves.java
+++ src/main/java/leetcode/oo/sort/MinDiffInThreeMoves.java
@@ -1,7 +1,6 @@
 package leetcode.oo.sort;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 
 //https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/
 final class MinDiffInThreeMoves {
@@ -10,26 +9,14 @@ final class MinDiffInThreeMoves {
         if (nums.length < 5) {
             return 0;
         }
-        final PriorityQueue<Integer> max = new PriorityQueue<>();
-        final PriorityQueue<Integer> min = new PriorityQueue<>(Comparator.reverseOrder());
-        for (int i = 0; i < nums.length; i++) {
-            max.offer(nums[i]);
-            min.offer(nums[i]);
-            if (max.size() > 4) {
-                max.poll();
-                min.poll();
-            }
-        }
-        final int[] minNums = new int[4];
-        final int[] maxNums = new int[4];
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
         for (int i = 0; i < 4; i++) {
-            maxNums[i] = max.poll();
-            minNums[3 - i] = min.poll();
+            min = Math.min(
+                min,
+                nums[nums.length - 4 + i] - nums[i]
+            );
         }
-        int minDiff = Integer.MAX_VALUE;
-        for (int i = 0; i < 4; i++) {
-            minDiff = Math.min(minDiff, maxNums[i] - minNums[i]);
-        }
-        return minDiff;
+        return min;
     }
 }
