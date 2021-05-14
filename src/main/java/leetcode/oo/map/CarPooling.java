92173b987e0fe9224440d9615186263dd7e9234e
strogiyotec <almas337519@gmail.com>
2021-05-14 07:09:04 -0700

    car pooling more readable

--- src/main/java/leetcode/oo/map/CarPooling.java
+++ src/main/java/leetcode/oo/map/CarPooling.java
@@ -10,13 +10,15 @@ final class CarPooling {
     boolean carPooling(int[][] trips, int capacity) {
         Arrays.sort(trips, Comparator.comparingInt(o -> o[1]));
         final PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
-        int currentPassangers = 0;
-        for (final int[] trip : trips) {
-            while (!queue.isEmpty() && trip[1] >= queue.peek()[2]) {
-                currentPassangers -= queue.poll()[0];
+        queue.add(trips[0]);
+        int currentCapacity = trips[0][0];
+        for (int i = 1; i < trips.length; i++) {
+            final int[] trip = trips[i];
+            while (!queue.isEmpty() && queue.peek()[2] <= trip[1]) {
+                currentCapacity -= queue.poll()[0];
             }
-            currentPassangers += trip[0];
-            if(currentPassangers>capacity){
+            currentCapacity += trip[0];
+            if(currentCapacity >capacity){
                 return false;
             }
             queue.offer(trip);
