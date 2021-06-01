fbf66facbddd91312148f0411547075d3bd75965
strogiyotec <almas337519@gmail.com>
2021-06-01 11:24:54 -0700

    zeros

--- src/main/java/leetcode/oo/pointers/MoveZeroes.java
+++ src/main/java/leetcode/oo/pointers/MoveZeroes.java
@@ -1,6 +1,5 @@
 package leetcode.oo.pointers;
 
-
 /**
  * Move zeroes.
  * See {@link <a href ="https://leetcode.com/problems/move-zeroes/">https://leetcode.com/problems/move-zeroes/</a>}
@@ -8,17 +7,25 @@ package leetcode.oo.pointers;
 final class MoveZeroes {
 
     void moveZeroes(int[] nums) {
-        if (nums.length == 0 || nums.length == 1) {
+        if (nums.length == 1) {
             return;
         }
-        int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[index++] = nums[i];
+        int zeroIndex = -1;
+        int nonZeroIndex = 0;
+        while (nonZeroIndex < nums.length) {
+            if (nums[nonZeroIndex] == 0) {
+                if (zeroIndex == -1) {
+                    zeroIndex = nonZeroIndex;
+                }
+                nonZeroIndex++;
+            } else if (zeroIndex != -1) {
+                final int temp = nums[nonZeroIndex];
+                nums[nonZeroIndex] = 0;
+                nums[zeroIndex] = temp;
+                zeroIndex++;
+            } else{
+                nonZeroIndex++;
             }
-        }
-        for (int i = index; i < nums.length; i++) {
-            nums[i] = 0;
         }
     }
 }
