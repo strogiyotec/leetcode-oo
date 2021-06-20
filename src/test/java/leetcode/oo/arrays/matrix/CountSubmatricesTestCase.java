1b274898e60a92f948816a315e69df786d93bdee
strogiyotec <almas337519@gmail.com>
2021-06-20 11:45:41 -0700

    more tests

--- src/test/java/leetcode/oo/arrays/matrix/CountSubmatricesTestCase.java
+++ src/test/java/leetcode/oo/arrays/matrix/CountSubmatricesTestCase.java
@@ -29,4 +29,31 @@ public final class CountSubmatricesTestCase {
             )
         );
     }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(
+            5,
+            this.alg.numSubmat(
+                new int[][]{
+                    {1, 0, 1},
+                    {0, 1, 0},
+                    {1, 0, 1}
+                }
+            )
+        );
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertEquals(
+            17,
+            this.alg.numSubmat(
+                new int[][]{
+                    {1, 1, 1, 1, 0}, {1, 0, 0, 1, 0}, {0, 0, 1, 0, 1}, {0, 1, 0, 0, 0}
+                }
+            )
+        );
+    }
+
 }
--- src/test/java/leetcode/oo/map/HIndexTestCase.java
+++ src/test/java/leetcode/oo/map/HIndexTestCase.java
@@ -12,12 +12,12 @@ public final class HIndexTestCase {
      * Init.
      */
     @Before
-    public void init(){
+    public void init() {
         this.alg = new HIndex();
     }
 
     @Test
-    public void test(){
+    public void test() {
         Assert.assertEquals(
             3,
             this.alg.hIndex(
@@ -25,4 +25,15 @@ public final class HIndexTestCase {
             )
         );
     }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(
+            1,
+            this.alg.hIndex(
+                new int[]{1, 1, 3}
+            )
+        );
+    }
+
 }
