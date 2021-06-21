b1a2641fe0e1aaf191ee937e259fc9103426ae1b
strogiyotec <almas337519@gmail.com>
2021-06-21 08:11:24 -0700

    improve budget

--- src/main/java/leetcode/oo/window/StrWithinBudget.java
+++ src/main/java/leetcode/oo/window/StrWithinBudget.java
@@ -4,28 +4,41 @@ package leetcode.oo.window;
 public final class StrWithinBudget {
 
     public static void main(String[] args) {
-        System.out.println(new StrWithinBudget().equalSubstring(
-            "abcd",
-            "bcdf",
-            3
-        ));
-        System.out.println(new StrWithinBudget().equalSubstring(
-            "abcd",
-            "cdef",
-            3
-        ));
+        System.out.println(
+            new StrWithinBudget().equalSubstring(
+                "krpgjbjjznpzdfy",
+                "nxargkbydxmsgby",
+                14
+            )
+        );
+        System.out.println(
+            new StrWithinBudget().equalSubstring(
+                "abcd",
+                "cdef",
+                3
+            )
+        );
+        System.out.println(
+            new StrWithinBudget().equalSubstring(
+                "abcd",
+                "bcdf",
+                3
+            )
+        );
     }
 
     public int equalSubstring(String s, String t, int maxCost) {
         int left = 0;
-        int currentLength = 0;
-        for (int right = 0; right < s.length(); right++) {
-            currentLength += Math.abs(s.charAt(right) - t.charAt(right));
-            if (currentLength > maxCost) {
-                currentLength -= Math.abs(s.charAt(left) - t.charAt(left));
+        int right = 0;
+        int currentCost = 0;
+        while (right < s.length()) {
+            currentCost += Math.abs(s.charAt(right) - t.charAt(right));
+            if (currentCost > maxCost) {
+                currentCost -= Math.abs(s.charAt(left) - t.charAt(left));
                 left++;
             }
+            right++;
         }
-        return s.length() - left;
+        return s.length()-left;
     }
 }
