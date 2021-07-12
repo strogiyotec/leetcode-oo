b9ec29eac7dbd302309759671b9a6e9fefd41a27
strogiyotec <almas337519@gmail.com>
2021-07-12 07:20:29 -0700

    readable wiggle sort

--- src/main/java/leetcode/oo/sort/WiggleSortII.java
+++ src/main/java/leetcode/oo/sort/WiggleSortII.java
@@ -8,21 +8,25 @@ final class WiggleSortII {
     void wiggleSort(int[] nums) {
         Arrays.sort(nums);
         final int[] temp = Arrays.copyOf(nums, nums.length);
-        int middle;
+        int middle = nums.length / 2;
         if (nums.length % 2 == 0) {
-            middle = nums.length / 2 - 1;
-        } else {
-            middle = nums.length / 2;
-        }
-        int right = nums.length - 1;
-        for (int i = 0; i < nums.length; i += 2) {
-            nums[i] = temp[middle];
-            if (i + 1 >= nums.length) {
-                break;
-            }
-            nums[i + 1] = temp[right];
             middle--;
-            right--;
         }
+        int left = middle;
+        int right = nums.length - 1;
+        int index = 0;
+        while (left >= 0 && right != middle && index < temp.length) {
+            temp[index] = nums[left];
+            temp[index + 1] = nums[right];
+            right--;
+            left--;
+            index += 2;
+        }
+        while (left >= 0 && index < temp.length) {
+            temp[index] = nums[left];
+            left++;
+            index++;
+        }
+        System.arraycopy(temp, 0, nums, 0, nums.length);
     }
 }
