c723d72eb848fa16e771c9bd1548a9ee370d4770
strogiyotec <almas337519@gmail.com>
2021-07-29 08:38:12 -0700

    more test cases

--- src/test/java/leetcode/oo/tree/FromInorderAndPostTestCase.java
+++ src/test/java/leetcode/oo/tree/FromInorderAndPostTestCase.java
@@ -12,20 +12,30 @@ public final class FromInorderAndPostTestCase {
      * Init.
      */
     @Before
-    public void init(){
+    public void init() {
         this.alg = new FromInorderPostOrder();
     }
 
     @Test
-    public void test(){
+    public void test() {
         final PlainTree tree = this.alg.buildTree(
             new int[]{2, 1, 4, 3, 5},
             new int[]{2, 4, 5, 3, 1}
         );
-        Assert.assertEquals(1,tree.val);
-        Assert.assertEquals(2,tree.left.val);
-        Assert.assertEquals(3,tree.right.val);
-        Assert.assertEquals(4,tree.right.left.val);
-        Assert.assertEquals(5,tree.right.right.val);
+        Assert.assertEquals(1, tree.val);
+        Assert.assertEquals(2, tree.left.val);
+        Assert.assertEquals(3, tree.right.val);
+        Assert.assertEquals(4, tree.right.left.val);
+        Assert.assertEquals(5, tree.right.right.val);
+    }
+
+    @Test
+    public void test2() {
+        final PlainTree tree = this.alg.buildTree(
+            new int[]{2, 1},
+            new int[]{2, 1}
+        );
+        Assert.assertEquals(1, tree.val);
+        Assert.assertEquals(2, tree.left.val);
     }
 }
