9ff19a1b073b28a4ef388992a854a03cb323cddf
strogiyotec <almas337519@gmail.com>
2021-09-02 09:00:28 -0700

    predict winner

--- src/main/java/leetcode/oo/dp/PredictWinner.java
+++ src/main/java/leetcode/oo/dp/PredictWinner.java
@@ -1,54 +1,35 @@
 package leetcode.oo.dp;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.stream.IntStream;
 
 //https://leetcode.com/problems/predict-the-winner/
 final class PredictWinner {
 
     boolean PredictTheWinner(int[] nums) {
-        final Map<Pair, Integer> cache = new HashMap<>(nums.length);
-        return this.dfs(nums, 0, nums.length - 1, cache) >= 0;
+        final int sum = IntStream.of(nums).sum();
+        final int firstScore = this.dfs(nums, 0, nums.length-1);
+        return firstScore >= sum-firstScore;
     }
 
-    private int dfs(final int[] nums, final int left, final int right, final Map<Pair, Integer> cache) {
+    private int dfs(final int[] nums, final int left, final int right) {
         if (left == right) {
             return nums[left];
         }
-        if (left > right) {
+        if (left > right || right<0) {
             return 0;
         }
-        final int fromLeft = nums[left] - this.dfs(nums, left + 1, right, cache);
-        final int fromRight = nums[right] - this.dfs(nums, left, right - 1, cache);
-        return Math.max(
-            fromLeft,
-            fromRight
+        int fromLeft = nums[left] + Math.min(
+            this.dfs(nums, left + 2, right),
+            this.dfs(nums, left + 1, right - 1)
         );
-    }
-
-    private static class Pair {
-        final int left;
-        final int right;
-
-        private Pair(final int left, final int right) {
-            this.left = left;
-            this.right = right;
-        }
-
-        @Override
-        public boolean equals(final Object o) {
-            if (this == o) return true;
-            if (o == null || !this.getClass().equals(o.getClass()))
-                return false;
-            final Pair pair = (Pair) o;
-            return this.left == pair.left && this.right == pair.right;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(this.left, this.right);
-        }
+        int fromRight = nums[right] + Math.min(
+            this.dfs(nums, left, right - 2),
+            this.dfs(nums,left + 1, right - 1)
+        );
+        return Math.max(
+            fromRight,
+            fromLeft
+        );
     }
 
 }
