fed2332b2821d1c0322ef0c58ba3b038ecfc4eb5
strogiyotec <almas337519@gmail.com>
2021-12-28 09:50:06 -0800

    partition equal

--- src/main/java/leetcode/oo/graphs/PartitionEqualSum.java
+++ src/main/java/leetcode/oo/graphs/PartitionEqualSum.java
@@ -1,6 +1,5 @@
 package leetcode.oo.graphs;
 
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 //https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
@@ -13,28 +12,42 @@ final class PartitionEqualSum {
         }
         final int sumPerPart = sum / k;
         final boolean[] visited = new boolean[nums.length];
-        Arrays.sort(nums);
-        return this.dfs(k, sumPerPart, visited, nums.length - 1, 0, nums);
+        final Search search = new Search() {
+            @Override
+            public boolean dfs(
+                final boolean[] visited,
+                final int[] nums,
+                final int parts,
+                final int index,
+                final int currentSum
+            ) {
+                if (parts == 0) {
+                    return true;
+                }
+                if (currentSum == sumPerPart) {
+                    return this.dfs(visited, nums, parts - 1, 0, 0);
+                }
+                if (index >= nums.length || currentSum > sumPerPart) {
+                    return false;
+                }
+                for (int i = index; i < nums.length; i++) {
+                    if (!visited[i]) {
+                        visited[i] = true;
+                        if (this.dfs(visited, nums, parts, i + 1, currentSum + nums[i])) {
+                            return true;
+                        }
+                        visited[i] = false;
+                    }
+                }
+                return false;
+            }
+        };
+
+        return search.dfs(visited, nums, k, 0, 0);
     }
 
-    private boolean dfs(final int k, final int sumPerPart, final boolean[] visited, final int index, final int currentSum, final int[] nums) {
-        if (k == 0) {
-            return true;
-        }
-        if (currentSum == sumPerPart) {
-            return this.dfs(k - 1, sumPerPart, visited, nums.length - 1, 0, nums);
-        }
-        for (int i = index; i >= 0; i--) {
-            if (!visited[i] && nums[i] + currentSum <= sumPerPart) {
-                visited[i] = true;
-                if (this.dfs(k, sumPerPart, visited, i - 1, currentSum + nums[i], nums)) {
-                    return true;
-                } else {
-                    visited[i] = false;
-                }
-            }
-        }
-        return false;
+    interface Search {
+        boolean dfs(boolean[] visited, int[] nums, int parts, int index, int currentSum);
     }
 
 }
