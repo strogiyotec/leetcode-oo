25782761e2970cca238530f9b894628b3d0143f7
strogiyotec <almas337519@gmail.com>
2022-04-18 10:47:21 -0700

    min cost ticket

--- src/main/java/leetcode/oo/dp/MinCostForTicket.java
+++ src/main/java/leetcode/oo/dp/MinCostForTicket.java
@@ -1,32 +1,31 @@
 package leetcode.oo.dp;
 
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.stream.IntStream;
 
 //https://leetcode.com/problems/minimum-cost-for-tickets/
 public final class MinCostForTicket {
 
     public int mincostTickets(int[] days, int[] costs) {
-        final Queue<int[]> day7 = new LinkedList<>();
-        final Queue<int[]> day30 = new LinkedList<>();
-        int cost = 0;
+        final Queue<int[]> queue7 = new ArrayDeque<>(days.length);
+        final Queue<int[]> queue30 = new ArrayDeque<>(days.length);
+        int totalCost = 0;
         for (final int day : days) {
-            while (!day7.isEmpty() && day7.peek()[0] + 7 <= day) {
-                day7.poll();
+            while (!queue7.isEmpty() && queue7.peek()[0] + 7 <= day) {
+                queue7.poll();
             }
-            day7.offer(new int[]{day, cost + costs[1]});
-            while (!day30.isEmpty() && day30.peek()[0] + 30 <= day) {
-                day30.poll();
+            while (!queue30.isEmpty() && queue30.peek()[0] + 30 <= day) {
+                queue30.poll();
             }
-            day30.offer(new int[]{day, cost + costs[2]});
-            cost = Math.min(
-                cost + costs[0],
-                Math.min(
-                    day7.peek()[1],
-                    day30.peek()[1]
-                )
-            );
+            queue7.add(new int[]{day, totalCost + costs[1]});
+            queue30.add(new int[]{day, totalCost + costs[2]});
+            totalCost = IntStream.of(
+                totalCost+costs[0],
+                queue7.peek()[1],
+                queue30.peek()[1]
+            ).min().getAsInt();
         }
-        return cost;
+        return totalCost;
     }
 }
