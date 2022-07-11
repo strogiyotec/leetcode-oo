1e65740539344c379903822fc018b6b8ba96afbf
strogiyotec <almas337519@gmail.com>
2022-07-11 08:04:20 -0700

    more readable apples problem

--- src/main/java/leetcode/oo/graphs/MinCostApples.java
+++ src/main/java/leetcode/oo/graphs/MinCostApples.java
@@ -10,35 +10,41 @@ import java.util.Set;
 final class MinCostApples {
 
     int minTime(final int size, final int[][] edges, final List<Boolean> hasApple) {
-        final Map<Integer, Set<Integer>> adjacent = new HashMap<>();
-        for (int i = 0; i < size; i++) {
-            adjacent.put(i, new HashSet<>());
-        }
-        for (final int[] edge : edges) {
-            adjacent.get(edge[0]).add(edge[1]);
-            adjacent.get(edge[1]).add(edge[0]);
-        }
+        //key is an int , value is a set of adjacent nodes
+        final Map<Integer, Set<Integer>> adjacentMap = new HashMap<>();
+        this.buildMap(size, edges, adjacentMap);
         final Set<Integer> visited = new HashSet<>();
-        return this.dfs(adjacent, visited, hasApple, 0, 0);
+        return this.dfs(adjacentMap, visited, 0, hasApple);
     }
 
-    private int dfs(final Map<Integer, Set<Integer>> adjacent,
-        final Set<Integer> visited,
-        final List<Boolean> hasApple,
-        final int currentNode,
-        final int cost
-    ) {
-        if (visited.contains(currentNode)) {
-            return 0;
+    private void buildMap(final int size, final int[][] edges, final Map<Integer, Set<Integer>> adjacentMap) {
+        for (int i = 0; i < size; i++) {
+            adjacentMap.put(i, new HashSet<>());
         }
-        visited.add(currentNode);
-        int totalCost = 0;
-        for (final Integer vertex : adjacent.get(currentNode)) {
-            totalCost += this.dfs(adjacent, visited, hasApple, vertex, 2);
+        for (final int[] edge : edges) {
+            adjacentMap.get(edge[0]).add(edge[1]);
+            adjacentMap.get(edge[1]).add(edge[0]);
         }
-        if (totalCost == 0 && !hasApple.get(currentNode)) {
-            return 0;
-        }
-        return totalCost + cost;
     }
+
+    private int dfs(final Map<Integer, Set<Integer>> adjacent, final Set<Integer> visited, final int currentApple, final List<Boolean> hasApple) {
+        if (visited.contains(currentApple)) {
+            return 0;
+        }
+        visited.add(currentApple);
+        int cost = 0;
+        for (final Integer nextApple : adjacent.get(currentApple)) {
+            cost += this.dfs(adjacent, visited, nextApple, hasApple);
+        }
+        if (!hasApple.get(currentApple) && cost == 0) {
+            return 0;
+        } else {
+            if (currentApple == 0) {
+                return cost;
+            } else {
+                return 2 + cost;
+            }
+        }
+    }
+
 }
