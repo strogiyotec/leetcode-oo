ab87dc66c32d03f53a1c995bf5a620253afb0a5c
strogiyotec <almas337519@gmail.com>
2022-12-28 09:02:07 -0800

    change max length repeated subarray

--- src/main/java/leetcode/oo/dp/backtracking/MaxLengthRepeatedSubArray.java
+++ src/main/java/leetcode/oo/dp/backtracking/MaxLengthRepeatedSubArray.java
@@ -1,6 +1,5 @@
 package leetcode.oo.dp.backtracking;
 
-
 /**
  * Maximum Length of Repeated Subarray.
  * See {@link <a href ="https://leetcode.com/problems/maximum-length-of-repeated-subarray/">https://leetcode.com/problems/maximum-length-of-repeated-subarray/</a>}
@@ -8,13 +7,13 @@ package leetcode.oo.dp.backtracking;
 final class MaxLengthRepeatedSubArray {
 
     int findLength(final int[] first, final int[] second) {
-        final int[][] dp = new int[first.length + 1][second.length + 1];
+        final int[][] memo = new int[first.length + 1][second.length + 1];
         int max = 0;
-        for (int i = 1; i <= first.length; i++) {
-            for (int j = 1; j <= second.length; j++) {
-                if (first[i - 1] == second[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                    max = Math.max(max, dp[i][j]);
+        for (int i = first.length - 1; i >= 0; i--) {
+            for (int j = second.length - 1; j >= 0; j--) {
+                if (first[i] == second[j]) {
+                    memo[i][j] = 1 + memo[i + 1][j + 1];
+                    max = Math.max(max, memo[i][j]);
                 }
             }
         }
