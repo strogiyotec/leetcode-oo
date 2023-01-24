639d0c51719bce02d990ecedbd96f531087191db
strogiyotec <almas337519@gmail.com>
2023-01-23 22:41:33 -0800

    split ll

--- src/main/java/leetcode/oo/ll/SplitLinkedList.java
+++ src/main/java/leetcode/oo/ll/SplitLinkedList.java
@@ -5,30 +5,32 @@ import leetcode.oo.ListNode;
 //https://leetcode.com/problems/split-linked-list-in-parts/
 final class SplitLinkedList {
 
-    ListNode[] splitListToParts(final ListNode root, final int parts) {
+    ListNode[] splitListToParts(ListNode root, final int parts) {
+        final int size = this.getSize(root);
+        int biggerParts = size % parts;
+        final int sizePerPart = size / parts - 1;
+        final ListNode[] solution = new ListNode[parts];
+        for (int i = 0; i < parts; i++) {
+            ListNode temp = root;
+            for (int j = 0; j < (biggerParts == 0 ? sizePerPart : sizePerPart + 1) && root != null; j++) {
+                root = root.next;
+            }
+            biggerParts = Math.max(0, biggerParts - 1);
+            if (root != null) {
+                final ListNode next = root.next;
+                root.next = null;
+                root = next;
+            }
+            solution[i] = temp;
+        }
+        return solution;
+    }
+
+    private int getSize(final ListNode root) {
         int size = 0;
         for (ListNode temp = root; temp != null; temp = temp.next) {
             size++;
         }
-        int biggerParts = size % parts;
-        int partsSize = size / parts -1;
-        int index = 0;
-        final ListNode[] splitted = new ListNode[parts];
-        ListNode head = root;
-        while (head != null) {
-            ListNode tail = head;
-            final int addOne = biggerParts > 0 ? 1 : 0;
-            for (int i = 0; i < partsSize + addOne && tail != null; i++) {
-                tail = tail.next;
-            }
-            if (tail != null) {
-                final ListNode nextHead = tail.next;
-                tail.next = null;
-                splitted[index++] = head;
-                head = nextHead;
-                biggerParts--;
-            }
-        }
-        return splitted;
+        return size;
     }
 }
--- src/test/java/leetcode/oo/ll/SplitLLTestCase.java
+++ src/test/java/leetcode/oo/ll/SplitLLTestCase.java
@@ -2,6 +2,7 @@ package leetcode.oo.ll;
 
 import java.util.Arrays;
 import leetcode.oo.ListNode;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,13 +23,25 @@ public final class SplitLLTestCase {
         final ListNode[] listNodes = this.alg.splitListToParts(
             new ListNode(1).withNext(2).withNext(3), 5
         );
-        System.out.println(Arrays.toString(listNodes));
+        Assert.assertEquals(
+            "[1, 2, 3, null, null]",
+            Arrays.toString(listNodes)
+        );
     }
 
     @Test
     public void test2() {
         final ListNode[] listNodes = this.alg.splitListToParts(
             new ListNode(1).withNext(2).withNext(3).withNext(4).withNext(5).withNext(6).withNext(7).withNext(8).withNext(9).withNext(10),
+            3
+        );
+        System.out.println(Arrays.toString(listNodes));
+    }
+
+    @Test
+    public void test3() {
+        final ListNode[] listNodes = this.alg.splitListToParts(
+            new ListNode(0).withNext(1).withNext(2),
             3
         );
         System.out.println(Arrays.toString(listNodes));
