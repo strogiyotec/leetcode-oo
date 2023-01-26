d7baf41e260f45208daa9e6d96387781fe377498
strogiyotec <almas337519@gmail.com>
2023-01-25 22:26:41 -0800

    next greater element 2

--- src/main/java/leetcode/amazon/NextGreaterElementII.java
+++ src/main/java/leetcode/amazon/NextGreaterElementII.java
@@ -8,19 +8,19 @@ final class NextGreaterElementII {
     public int[] nextGreaterElements(int[] nums) {
         final Stack<Integer> stack = new Stack<>();
         for (int i = nums.length - 1; i >= 0; i--) {
-            stack.push(i);
+            stack.push(nums[i]);
         }
         final int[] nextGreater = new int[nums.length];
         for (int i = nums.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
+            while (!stack.isEmpty() && stack.peek() <= nums[i]) {
                 stack.pop();
             }
             if (stack.isEmpty()) {
                 nextGreater[i] = -1;
             } else {
-                nextGreater[i] = nums[stack.peek()];
+                nextGreater[i] = stack.peek();
             }
-            stack.push(i);
+            stack.push(nums[i]);
         }
         return nextGreater;
     }
--- src/test/java/leetcode/amazon/NextGreaterElementIITestCase.java
+++ src/test/java/leetcode/amazon/NextGreaterElementIITestCase.java
@@ -17,9 +17,29 @@ public final class NextGreaterElementIITestCase {
     }
 
     @Test
+    public void test3() {
+        Assert.assertArrayEquals(
+            new int[]{2, 3, 4, -1, 4},
+            this.alg.nextGreaterElements(
+                new int[]{1, 2, 3, 4, 3}
+            )
+        );
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertArrayEquals(
+            new int[]{2, 3, -1, 3, 2},
+            this.alg.nextGreaterElements(
+                new int[]{1, 2, 3, 2, 1}
+            )
+        );
+    }
+
+    @Test
     public void test() {
         Assert.assertArrayEquals(
-            new int[]{2,-1,2},
+            new int[]{2, -1, 2},
             this.alg.nextGreaterElements(
                 new int[]{1, 2, 1}
             )
