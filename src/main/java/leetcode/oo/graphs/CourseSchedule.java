4dc1d5e1d341a5bcedce9842d9312e52be687b5a
strogiyotec <almas337519@gmail.com>
2023-05-31 10:16:24 -0700

    course schedule

--- src/main/java/leetcode/oo/graphs/CourseSchedule.java
+++ src/main/java/leetcode/oo/graphs/CourseSchedule.java
@@ -1,47 +1,53 @@
 package leetcode.oo.graphs;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 //https://leetcode.com/problems/course-schedule/
 final class CourseSchedule {
 
     boolean canFinish(final int numCourses, int[][] prerequisites) {
-        final Map<Integer, Set<Integer>> adjacent = new HashMap<>();
+        final Map<Integer, List<Integer>> graph = new HashMap<>(numCourses);
         for (final int[] prerequisite : prerequisites) {
-            adjacent.merge(
-                prerequisite[1],
-                new HashSet<>(
-                    Collections.singletonList(prerequisite[0])
-                ),
-                (oldVal, newVal) -> {
-                    newVal.addAll(oldVal);
-                    return newVal;
-                }
-            );
+            graph.merge(prerequisite[0], new ArrayList<>(List.of(prerequisite[1])), (oldV, newV) -> {
+                oldV.addAll(newV);
+                return oldV;
+            });
         }
+        final Map<Integer, Boolean> cache = new HashMap<>(numCourses << 1);
         for (int i = 0; i < numCourses; i++) {
-            if (!this.dfs(i, new boolean[numCourses + 1], adjacent)) {
+            if (!this.dfs(i, new boolean[numCourses + 1], graph, cache)) {
                 return false;
             }
         }
         return true;
     }
 
-    private boolean dfs(final int node, final boolean[] visited, final Map<Integer, Set<Integer>> adjacent) {
-        if (visited[node]) {
+    private boolean dfs(final int index, final boolean[] visited, final Map<Integer, List<Integer>> graph, final Map<Integer, Boolean> cache) {
+        if (visited[index]) {
             return false;
         }
-        visited[node] = true;
-        for (final Integer vertex : adjacent.getOrDefault(node, Collections.emptySet())) {
-            if(!this.dfs(vertex,visited,adjacent)){
+        if (cache.containsKey(index)) {
+            return cache.get(index);
+        }
+        visited[index] = true;
+        for (final Integer vertex : graph.getOrDefault(index, Collections.emptyList())) {
+            if (!this.dfs(vertex, visited, graph, cache)) {
                 return false;
             }
         }
-        visited[node] = false;
+//          let's say
+//          3 ->1, 2
+//          1 ->0
+//          2 ->0
+//          when we go to 1 from 3 and then to 0
+//          then we need to set 0 back to false
+//          otherwise when we go there from 2 it will be true
+        visited[index] = false;
+        cache.put(index, true);
         return true;
     }
 
